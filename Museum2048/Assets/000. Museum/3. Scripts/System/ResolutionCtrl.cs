5232cc45bc027900e3a0502eaa620d1735c0823c
jazzgazer <radiogaga.jin@gmail.com>
2019-05-08 17:01:47 +0900

    Resolution 이슈 진행

---
+++ Museum2048/Assets/000. Museum/3. Scripts/System/ResolutionCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionCtrl : MonoBehaviour
+{
+    void Awake() {
+
+        Application.targetFrameRate = 30;
+        InitScreenResolution();
+    }
+
+
+    /// <summary>
+    /// 스크린 해상도 설정 
+    /// </summary>
+    public void InitScreenResolution() {
+
+        Debug.Log("!!!! InitScreenResolution !!! :: " + Screen.width + " / " + Screen.height);
+        float t = (float)Screen.width / (float)Screen.height;
+
+        Debug.Log("!! Aspect Ratio :: " + t);
+
+
+
+        if (t < 0.49f) { // 9:16 이상의 세로 해상도들. 
+
+            Debug.Log("This is a portrait tall device");
+#if UNITY_ANDROID
+
+            Screen.SetResolution(720, 1280, true);
+#endif
+
+
+        }
+
+        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+    }
+
+}
