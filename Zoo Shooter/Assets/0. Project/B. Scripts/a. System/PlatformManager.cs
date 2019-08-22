8cdd3f79ab6c288a249987673dcffc1c7b8de8be
jazzgazer <radiogaga.jin@gmail.com>
2019-08-22 16:05:53 +0900

    Facebook SDK

--- Zoo Shooter/Assets/0. Project/B. Scripts/a. System/PlatformManager.cs
+++ Zoo Shooter/Assets/0. Project/B. Scripts/a. System/PlatformManager.cs
@@ -11,7 +11,7 @@ using UnityEngine.SocialPlatforms.GameCenter;
 
 
 using UnityEngine;
-
+using Facebook.Unity;
 
 
 public class PlatformManager : MonoBehaviour {
@@ -23,7 +23,41 @@ public class PlatformManager : MonoBehaviour {
 
     private void Awake() {
         main = this;
+
+        if (!FB.IsInitialized) {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
     }
+    #region Facebook 
+
+    private void InitCallback() {
+        if (FB.IsInitialized) {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+        }
+    }
+
+    private void OnHideUnity(bool isGameShown) {
+        if (!isGameShown) {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+    #endregion
 
 
     // Start is called before the first frame update
