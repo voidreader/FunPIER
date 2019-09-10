511ed0e98266b6a6739f0686780a7eeb0a164eff
jazzgazer <radiogaga.jin@gmail.com>
2019-09-10 16:46:46 +0900

    업로드

--- Zoo Shooter/Assets/0. Project/B. Scripts/a. System/AdsManager.cs
+++ Zoo Shooter/Assets/0. Project/B. Scripts/a. System/AdsManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using GoogleMobileAds.Api;
 
 using System;
 
@@ -20,6 +20,20 @@ public class AdsManager : MonoBehaviour {
     [Header("- IronSource -")]
     public string IronSource_Android_ID;
     public string IronSource_iOS_ID;
+
+
+    #region 애드몹 
+    [Header("- Google Admob -")]
+    private BannerView bannerView = null;
+    private GoogleMobileAds.Api.InterstitialAd interstitial = null;
+    private RewardedAd rewardedAd = null;
+    public bool isBannerActivated = false;
+
+    public string admob_android_appID, admob_ios_appID;
+    public string admob_android_bannerID, admob_android_interstitialID, admob_android_rewardedID;
+    public string admob_ios_bannerID, admob_ios_interstitialID, admob_ios_rewardedID;
+
+    #endregion
 
     #region Unity Ads
     /*
@@ -184,6 +198,216 @@ public class AdsManager : MonoBehaviour {
     }
 
 
+
+    #region 배너
+
+    private void RequestBanner() {
+
+        if (PIER.IsSpecialist)
+            return;
+
+#if UNITY_ANDROID
+        string adUnitId = admob_android_bannerID;
+#elif UNITY_IPHONE
+            string adUnitId = admob_ios_bannerID;
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        bannerView = new BannerView(adUnitId, GoogleMobileAds.Api.AdSize.Banner, GoogleMobileAds.Api.AdPosition.Bottom);
+
+        // Called when an ad request has successfully loaded.
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        bannerView.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is clicked.
+        bannerView.OnAdOpening += HandleOnAdOpened;
+        // Called when the user returned from the app after an ad click.
+        bannerView.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        bannerView.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args) {
+        Debug.Log("HandleAdLoaded event received");
+
+        isBannerActivated = true;
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args) {
+        Debug.Log("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+
+        isBannerActivated = false;
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args) {
+        Debug.Log("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args) {
+        Debug.Log("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args) {
+        Debug.Log("HandleAdLeavingApplication event received");
+    }
+
+#endregion
+
+    #region 전면
+    private void RequestInterstitial() {
+
+
+        if (this.interstitial != null && this.interstitial.IsLoaded())
+            return;
+
+
+#if UNITY_ANDROID
+        string adUnitId = admob_android_interstitialID;
+#elif UNITY_IPHONE
+        string adUnitId = admob_ios_interstitialID;
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new GoogleMobileAds.Api.InterstitialAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += InterHandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += InterHandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += InterHandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += InterHandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += InterHandleOnAdLeavingApplication;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+
+
+
+
+
+    }
+
+    public void InterHandleOnAdLoaded(object sender, EventArgs args) {
+        Debug.Log("HandleAdLoaded event received");
+    }
+
+    public void InterHandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args) {
+        Debug.Log("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void InterHandleOnAdOpened(object sender, EventArgs args) {
+        Debug.Log("HandleAdOpened event received");
+    }
+
+    public void InterHandleOnAdClosed(object sender, EventArgs args) {
+        Debug.Log("HandleAdClosed event received");
+        RequestInterstitial();
+    }
+
+    public void InterHandleOnAdLeavingApplication(object sender, EventArgs args) {
+        Debug.Log("HandleAdLeavingApplication event received");
+    }
+    #endregion
+
+    #region 애드몹 동영상
+
+    public void RequestRewardAd() {
+
+
+        string adUnitId;
+#if UNITY_ANDROID
+        adUnitId = admob_android_rewardedID;
+#elif UNITY_IPHONE
+            adUnitId = admob_ios_rewardedID;
+#else
+            adUnitId = "unexpected_platform";
+#endif
+
+        this.rewardedAd = new RewardedAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+        // Called when an ad request failed to load.
+        this.rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+        // Called when an ad is shown.
+        this.rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+        // Called when an ad request failed to show.
+        this.rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+        // Called when the user should be rewarded for interacting with the ad.
+        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        // Called when the ad is closed.
+        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
+    }
+
+    public void HandleRewardedAdLoaded(object sender, EventArgs args) {
+        Debug.Log("HandleRewardedAdLoaded event received");
+    }
+
+    public void HandleRewardedAdFailedToLoad(object sender, AdErrorEventArgs args) {
+        Debug.Log("HandleRewardedAdFailedToLoad event received with message: " + args.Message);
+    }
+
+    public void HandleRewardedAdOpening(object sender, EventArgs args) {
+        Debug.Log("HandleRewardedAdOpening event received");
+
+        // 애드몹이 오픈되도 소리가 자꾸 나와... 
+#if UNITY_IOS
+        if(SoundControlSystem.BGM_Available) {
+            AudioAssistant.main.ChangeMusicVolume(0);
+        }
+
+#endif
+    }
+
+    public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args) {
+        Debug.Log(
+            "HandleRewardedAdFailedToShow event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardedAdClosed(object sender, EventArgs args) {
+        Debug.Log("HandleRewardedAdClosed event received");
+
+
+#if UNITY_IOS
+        if (SoundControlSystem.BGM_Available) {
+            AudioAssistant.main.ChangeMusicVolume(0.6f);
+        }
+
+#endif
+
+        RequestRewardAd();
+    }
+
+    public void HandleUserEarnedReward(object sender, Reward args) {
+        Debug.Log(">> HandleUserEarnedReward << ");
+        OnWatchReward(); // callback 호출 
+
+        
+    }
+
+#endregion
 
 
 
