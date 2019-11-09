bf83c2442c8f23cf9975d679e62d922a5483bde4
Radiogaga.jin <radiogaga.jin@gmail.com>
2019-11-09 23:55:31 +0900

    !!!

--- ElectroCity/Assets/0. ElectroCity/C. Scripts/c. UI/Views/ViewBossCall.cs
+++ ElectroCity/Assets/0. ElectroCity/C. Scripts/c. UI/Views/ViewBossCall.cs
@@ -16,9 +16,28 @@ public class ViewBossCall : MonoBehaviour
 
     public List<Image> ListFrame; // 상단 초상화
 
+    public bool isStopped = false;
+
+
+    // 선택된 보스 정보 
+    public Text TextBossName;
+    public Image GradeTag; 
+
+    [Header("Moving Objects")]
+    public Transform MommyHead; // 엄마 머리
+    public Image LightOn; // 우측 불빛
+    public GameObject ButtonStopOn; // STOP 버튼
+
+    [SerializeField] int SelectedIndex = 0;
+    [SerializeField] BossDataRow SelectedBossData;
+    [SerializeField] float nextAlpha  = 0f;
+    [SerializeField] float incrementalValue;
+
     public void OnView() {
 
         Debug.Log("ViewBossCall OnView");
+
+        InitMovingObject();
 
         // Common 2개, Rare 1개, Unique 1개, Legendary 1개 뽑는다
         PickNominee();
@@ -28,16 +47,34 @@ public class ViewBossCall : MonoBehaviour
         StartCoroutine(Rolling());
     }
 
+    /// <summary>
+    /// 시작 전 초기화 
+    /// </summary>
+    void InitMovingObject() {
+        ButtonStopOn.SetActive(false);
+        // LightOn.gameObject.SetActive(false);
+        LightOn.DOKill();
+        LightOn.color = new Color(1, 1, 1, 0);
+        MommyHead.localRotation = Quaternion.identity;
+
+        TextBossName.text = string.Empty;
+        GradeTag.gameObject.SetActive(false);
+    }
+
 
     IEnumerator Rolling() {
         int rollIndex = 0;
         int order = 0;
+        int moveCount = 0;
         float speed = 1.4f;
 
-        while(true) {
+
+        isStopped = false;
+
+        while (!isStopped) {
             MoveNominee(rollIndex, ListRandomOrder[order], speed);
 
-
+            moveCount++;
             rollIndex++;
             order++;
 
@@ -51,16 +88,102 @@ public class ViewBossCall : MonoBehaviour
             if (speed < 0.2f)
                 speed = 0.2f;
 
+            if(moveCount > 12) {
+                SetReadyStop();
+            }
+
             yield return new WaitForSeconds(speed * 0.5f);
         }
 
         
     }
 
+
+    public void OnClickStop() {
+
+        isStopped = true;
+        ButtonStopOn.SetActive(false);
+
+        // 멈추기!
+        for(int i=0; i<ListNomineeSprite.Count;i++) {
+            ListNomineeSprite[i].transform.DOKill(); // 다 멈추고. 
+        }
+
+        ListNomineeSprite[SelectedIndex].transform.DOLocalMoveX(0, 1f).SetDelay(0.5f);
+
+        if (SelectedIndex - 1 < 0)
+            ListNomineeSprite[ListNomineeSprite.Count - 1].transform.DOLocalMoveX(-500, 0.5f).SetDelay(0.5f);
+        else
+            ListNomineeSprite[SelectedIndex - 1].transform.DOLocalMoveX(-500, 0.5f).SetDelay(0.5f);
+    }
+
+    /// <summary>
+    /// 버튼 및 불 활성화
+    /// </summary>
+    void SetReadyStop() {
+        ButtonStopOn.SetActive(true);
+        // LightOn.DOColor(new Color(1, 1, 1, 1), 1).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+        // LightOn.DOFade(1, 1).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+
+        StartCoroutine(Lighting());
+    }
+
+    IEnumerator Lighting() {
+        Color c = LightOn.color;
+
+        incrementalValue = 0.01f;
+
+        nextAlpha = 0;
+        c.a = nextAlpha;
+        LightOn.color = c;
+
+        while(!isStopped) {
+
+            if(nextAlpha > 1) {
+                nextAlpha = 1;
+                c.a = nextAlpha;
+                incrementalValue = -1 * incrementalValue;
+                Debug.Log("Lighting Backward!");
+            }
+            else if(nextAlpha < 0) {
+                nextAlpha = 0;
+                c.a = nextAlpha;
+                incrementalValue = -1 * incrementalValue;
+                Debug.Log("Lighting Forward!");
+            }
+
+            nextAlpha += incrementalValue;
+            c.a = nextAlpha;
+
+
+            
+            LightOn.color = c;
+            yield return null;
+
+
+        }
+
+        LightOn.color = new Color(1, 1, 1, 1);
+    }
+
+
+    /// <summary>
+    /// 하나씩 좌로 움직이기.
+    /// </summary>
+    /// <param name="i"></param>
+    /// <param name="row"></param>
+    /// <param name="speed"></param>
     void MoveNominee(int i, BossDataRow row, float speed) {
+
+        if (isStopped)
+            return;
+
         ListNomineeSprite[i].sprite = Stock.GetBossUI_Sprite(row._spriteUI);
         ListNomineeSprite[i].transform.localPosition = new Vector2(500, row._beltPosY); // 포지션 (Y값만 달라진다)
         ListNomineeSprite[i].transform.DOLocalMoveX(-500, speed).SetEase(Ease.Linear);
+
+        SelectedIndex = i;
+        SelectedBossData = row;
     }
 
     void OnMoveComplete(Transform t) {
